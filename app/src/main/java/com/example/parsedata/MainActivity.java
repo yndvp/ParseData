15c7a4e832315bf25b4c2b0b8c21a0c576ca00d0
yndvp <58580623+yndvp@users.noreply.github.com>
2022-02-01 21:47:52 -0500

    parse a JSONArray with JSONArrayRequest

--- app/src/main/java/com/example/parsedata/MainActivity.java
+++ app/src/main/java/com/example/parsedata/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.parsedata;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,40 +10,81 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    //our Target url
+public class MainActivity extends AppCompatActivity {
+    RequestQueue queue;
     String url = "https://www.google.com";
-    
+    String apiUrl = "https://jsonplaceholder.typicode.com/todos";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Initialising queue which will fetch all the data in the background so that we can live worry free mentos zindegi
-        RequestQueue queue = Volley.newRequestQueue(this);
+        queue = Volley.newRequestQueue(this);
 
+        JsonArrayRequest jsonArrayRequest = getJsonArrayRequest();
+
+        queue.add(jsonArrayRequest);
+
+        getString(queue);
+
+    }
+
+    @NonNull
+    private JsonArrayRequest getJsonArrayRequest() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, apiUrl, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                Log.d("JSON", "onCreate: " + jsonObject.getString("userId"));
+                                // In case that values are not saved as string, getString() will not will work.
+                                // Make sure to use the method that matches with the type of field
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("JSON", "onCreate: Failed!");
+
+            }
+        });
+        return jsonArrayRequest;
+    }
+
+    private void getString(RequestQueue queue) {
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         // Display the first 100 characters of the response string.
-                        Log.d("TAG", "Response is: " + response.substring(0, 100));
+                        Log.d("Main", "Response is: " + response.substring(0, 100));
                     }
                 }, new Response.ErrorListener() {
             @Override
             //In case of any error while fetching data we display this
             public void onErrorResponse(VolleyError error) {
-                Log.d("TAG", "That didn't work!");
+                Log.d("Main", "That didn't work!");
             }
         });
 
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
-
     }
 }
